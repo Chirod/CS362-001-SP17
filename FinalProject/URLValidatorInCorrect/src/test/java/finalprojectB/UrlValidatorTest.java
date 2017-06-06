b103b6d899b0d0a9a325d7a77ca593cc3349a612
Christopher <wohlwenc@oregonstate.edu>
2017-06-06 10:56:05 -0700

    c's work

--- FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -41,21 +41,91 @@ public class UrlValidatorTest extends TestCase {
    public void testManualTest()
    {
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	   System.out.println(urlVal.isValid("http://www.amazon.com"));
-	   
+//	   System.out.println(urlVal.isValid("http://www.am%30azon.com"));
+//	   System.out.println(urlVal.isValid("http://name:pass@www.pc-help.org/obscure.htm"));
 	   
    }
+   //Christopher - do scheme, path
+   private String assembleURL(String scheme, String authority, String port, String path, String query) {
+	   return (scheme == null ? "" : scheme) + 
+			   (authority == null ? "" : authority) + 
+			   (port == null ? "" : port) + 
+			   (path == null ? "" : path) + 
+			   (query == null ? "" : query);
+   }
    
+   private void testUrl(boolean shouldBeValid, String url) throws Exception {
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   assertEquals(shouldBeValid, urlVal.isValid(url));
+   }
    
-   public void testYourFirstPartition()
+   public void testScheme() throws Exception
    {
+	   // legal
+	   testUrl(true, assembleURL("http://","www.amazon.com", null, null, null));
+	   // all legal characters
+	   testUrl(true, assembleURL("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-.://","www.amazon.com", null, null, null));
+	   // other legal stuff
+	   testUrl(true, assembleURL("b234qh43://","www.amazon.com", null, null, null));
+	   testUrl(true, assembleURL("A://","www.amazon.com", null, null, null));
+	   testUrl(true, assembleURL("coolstuff://","www.amazon.com", null, null, null));
+	   
+	   //start with usually legal, but non-alpha char
+	   testUrl(false, assembleURL("1moretime://","www.amazon.com", null, null, null));
+	   
+	   // blank scheme with ://
+	   testUrl(false, assembleURL("://","www.amazon.com", null, null, null));
+	   
+	   testUrl(false, assembleURL("http:/","www.amazon.com", null, null, null));	   
+	   testUrl(false, assembleURL("http//","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("http:","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("http/","www.amazon.com", null, null, null));
+	   //invalid characters
+	   testUrl(false, assembleURL("ht*tp://","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("ht%tp://","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("ht$tp://","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("ht#tp://","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("ht@tp://","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("ht!tp://","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("ht~tp://","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("ht^tp://","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("ht(tp://","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("ht)tp://","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("ht{tp://","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("ht}tp://","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("ht\"tp://","www.amazon.com", null, null, null));
+	   testUrl(false, assembleURL("ht'tp://","www.amazon.com", null, null, null));
+   }
+   
+   public void testAuthority() throws Exception {
+	   testUrl(true, assembleURL("http://","username.password@www.amazon.com", null, null, null));
+   }
+   
+   public void testQuery() {
+	   
+   }
+
+   public void testPart() {
 	   
    }
    
-   public void testYourSecondPartition(){
+   public void testPath() throws Exception {
+	   testUrl(true, "http://www.amazon.com");
+	   testUrl(true, assembleURL("http://","www.amazon.com", null, "", null));
+	   testUrl(true, assembleURL("http://","www.amazon.com", null, "/", null));
+	   testUrl(true, assembleURL("http://","www.amazon.com", null, "/morestuff", null));
+	   testUrl(true, assembleURL("http://","www.amazon.com", null, "/encoded%20char", null));
+	   testUrl(true, assembleURL("http://","www.amazon.com", null, "/this/is/a/lot/of/paths/hopefully/it/is/long/enough/to/find/potential/errors/in/the/scenario/that/that/would/be/necessary/for/us/", null));
+	   testUrl(true, assembleURL("http://","www.amazon.com", null, "/done/", null));
+	   testUrl(true, assembleURL("http://","www.amazon.com", null, "/a/bc/def/ghij/klmno/pqrstu/vwxyz12/34567890/-._~%23%65%20", null));
 	   
+	   testUrl(false, assembleURL("http://","www.amazon.com", null, "/comming/up/is/invalid/char/asdf^asfd", null));
+	   testUrl(false, assembleURL("http://","www.amazon.com", null, "this/should/start/with/a/slash", null));
+	   testUrl(false, assembleURL("http://","www.amazon.com", null, "/more/invlid/chars/as<df", null));
+	   testUrl(false, assembleURL("http://","www.amazon.com", null, "/more/invlid/chars/as^df", null));
+	   testUrl(false, assembleURL("http://","www.amazon.com", null, "/more/invlid/chars/as{df", null));
+
    }
-   
    
    public void testIsValid()
    {
